9c4ef1434240755896c43af1b1a9a887c9e449c6
Vivek <vivekrajput2515@gmail.com>
2026-01-14 16:00:08 +0530

    add program of linq

---
+++ Assessment14-01-26/LINQ_Assignment_BoilerPlateCode/Program.cs
@@ -0,0 +1,166 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LINQ_Assignment_BoilerPlateCode.Repos;
+using LINQ_Assignment_BoilerPlateCode.DTOs;
+using LINQ_Assignment_BoilerPlateCode.Models;
+
+namespace LINQ_Assignment_BoilerPlateCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // =======================
+            // SAMPLE DATA
+            // =======================
+            var employees = EmployeeRepo.SeedEmployees();
+            var projects = ProjectRepo.SeedProjects();
+
+            Console.WriteLine("LINQ Scenario Boilerplate Loaded");
+
+            GetHighEarningEmployees(employees);
+        }
+
+        // =====================================================
+        // 🟢 SECTION 1 – HR ANALYTICS
+        // =====================================================
+
+        // TODO 1.1: Get employees earning more than 60,000
+        static List<Employee> GetHighEarningEmployees(List<Employee> employees)
+        {
+            // TODO: Write LINQ query here
+            var highEarningEmployees = employees.Where(e => e.Salary > 60000).ToList();
+            return highEarningEmployees;
+        }
+
+        // TODO 1.2: Get list of employee names only
+        static List<string> GetEmployeeNames(List<Employee> employees)
+        {
+            var getEmpName = employees.Select(e => e.Name).ToList();
+            return getEmpName;
+
+        }
+
+        // TODO 1.3: Check if any employee belongs to HR department
+        static bool HasHREmployees(List<Employee> employees)
+        {
+            // TODO: Write LINQ query here
+            var hasHREmp = employees.Any(e => e.Department == "HR");
+            return hasHREmp;
+
+        }
+
+        // =====================================================
+        // 🟡 SECTION 2 – MANAGEMENT INSIGHTS
+        // =====================================================
+
+        // TODO 2.1: Get department-wise employee count
+        static List<DepartmentCount> GetDepartmentWiseCount(List<Employee> employees)
+        {
+            // TODO: Write LINQ query here
+            var departments = employees.GroupBy(e => e.Department).ToList();
+
+            List<DepartmentCount> departCount = new List<DepartmentCount>();
+
+            foreach (var depart in departments)
+            {
+                departCount.Add(new DepartmentCount
+                {
+                    Department = depart.Key,
+                    Count = depart.Count()
+                });
+            }
+
+            return departCount;
+        }
+
+        // TODO 2.2: Find the highest paid employee
+        static Employee GetHighestPaidEmployee(List<Employee> employees)
+        {
+            // TODO: Write LINQ query here
+            var highPaidEmp = employees.OrderByDescending(e => e.Salary).FirstOrDefault();
+            return highPaidEmp;
+        }
+
+        // TODO 2.3: Sort employees by Salary (DESC), then Name (ASC)
+        static List<Employee> SortEmployeesBySalaryAndName(List<Employee> employees)
+        {
+            // TODO: Write LINQ query here
+            var sortEmpBySalAndName = employees.OrderByDescending(e => e.Salary).ThenBy(e => e.Name).ToList();
+            return sortEmpBySalAndName;
+        }
+
+        // =====================================================
+        // 🔵 SECTION 3 – PROJECT & SKILL INTELLIGENCE
+        // =====================================================
+
+        // TODO 3.1: Join employees with projects
+        static List<EmployeeProject> GetEmployeeProjectMappings(
+            List<Employee> employees,
+            List<Project> projects)
+        {
+            // TODO: Write LINQ query here
+            throw new NotImplementedException();
+        }
+
+        // TODO 3.2: Find employees who are NOT assigned to any project
+        static List<Employee> GetUnassignedEmployees(
+            List<Employee> employees,
+            List<Project> projects)
+        {
+            // TODO: Write LINQ query here
+            List<Employee> unassignEmp = new List<Employee>();
+            return unassignEmp;
+
+
+        }
+
+        // TODO 3.3: Get all unique skills across the organization
+        static List<string> GetAllUniqueSkills(List<Employee> employees)
+        {
+            // TODO: Write LINQ query here
+            var uniqueSkill = employees.SelectMany(e => e.Skills).Distinct().ToList();
+            return uniqueSkill;
+        }
+
+        // =====================================================
+        // 🔴 SECTION 4 – ADVANCED WORKFORCE ANALYTICS
+        // =====================================================
+
+        // TODO 4.1: Get top 3 highest-paid employees per department
+        static List<DepartmentTopEmployees> GetTopEarnersByDepartment(
+            List<Employee> employees)
+        {
+            // TODO: Write LINQ query here
+            List<DepartmentTopEmployees> top = new List<DepartmentTopEmployees>();
+            return top;
+
+        }
+
+        // TODO 4.2: Remove duplicate employees based on Id
+        static List<Employee> RemoveDuplicateEmployees(List<Employee> employees)
+        {
+            // TODO: Write LINQ query here
+            //List<Employee> dupEmp = new List<Employee>();
+
+            var dupEMp = employees.GroupBy(e => e.Id).Select(e => e.First()).ToList();
+            return dupEMp;
+        }
+
+        // TODO 4.3: Implement pagination
+        static List<Employee> GetEmployeesByPage(
+            List<Employee> employees,
+            int pageNumber,
+            int pageSize = 5)
+        {
+            // TODO: Write LINQ query here
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
