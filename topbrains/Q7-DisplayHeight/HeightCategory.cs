47a7c108375a1c9993cf6da875ec688b72b9ff55
Vivek <vivekrajput2515@gmail.com>
2026-01-22 21:34:54 +0530

    create a program to check height category

---
+++ topbrains/Q7-DisplayHeight/HeightCategory.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class HeightCategory
+{
+    public static string DisplayHeight(int heightCm)
+    {
+        if (heightCm < 150)
+            return "Short";
+        else if (heightCm < 180)
+            return "Average";
+        else
+            return "Tall";
+    }
+}
---
+++ topbrains/Q7-DisplayHeight/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Enter height in cm: ");
+        // Take input from console
+        int heightCm = int.Parse(Console.ReadLine());
+
+        // Call method
+        string result = HeightCategory.DisplayHeight(heightCm);
+
+
+        // Print output
+        Console.WriteLine(result);
+    }
+}
