e2a860ea0178e5c4172c4682f616a7ee89793f87
Vivek <vivekrajput2515@gmail.com>
2026-01-25 20:20:41 +0530

    move question to practice folder

---
+++ Practice/Q1/Program.cs
@@ -0,0 +1,66 @@
+﻿public class Computer
+{
+    public string Processor {get; set;}
+    public int RamSize{get; set;}
+    public int HardDiskSize{get; set;}
+    public int GraphicCard{get; set;}
+}
+
+public class Desktop: Computer
+{
+    public int MonitorSize{get; set;}
+    public int PowerSupplyVolt{get; set;}
+
+    public double DesktopPriceCalculation()
+    {
+        int processorCost = 0;
+        if(Processor == "i3")
+        {
+            processorCost = 1500;
+        }else if(Processor == "i5"){
+            processorCost = 3000;
+        }else if(Processor == "i7")
+        {
+            processorCost = 4500;
+        }
+        double price = processorCost +(RamSize * 200) + (HardDiskSize * 1500)+(GraphicCard * 2500)+(MonitorSize * 250)+(PowerSupplyVolt * 20);
+        return price;
+    }
+
+    // public enum ProcessorCost
+    // {
+    //     i3 = 1500,
+    //     i5 = 3000,
+    //     i7 = 4500
+    // }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        Desktop desktopObj = new Desktop();
+
+        Console.WriteLine("Enter the processor");
+        desktopObj.Processor = Console.ReadLine();
+
+
+        Console.WriteLine("Enter the ram size");
+        desktopObj.RamSize = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the hard disk size");
+        desktopObj.HardDiskSize = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the graphic card size");
+        desktopObj.GraphicCard = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the monitor size");
+        desktopObj.MonitorSize = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the power supply volt");
+        desktopObj.PowerSupplyVolt = int.Parse(Console.ReadLine());
+
+        //Output
+        Console.WriteLine($"Desktop price is {desktopObj.DesktopPriceCalculation()}");
+    }
+}
---
+++ Practice/Q4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; // Hint: Might be useful for deleting, but looping works too
+
+public class Case
+{
+    public int CaseNo { get; set; }
+    public string CaseCode { get; set; }
+    public string CaseContent { get; set; }
+    public DateTime Date { get; set; }
+}
+
+public class Program
+{
+    public static List<Case> CaseFile = new List<Case>();
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("1. Add to the list\n2. Delete from the list");
+
+        Console.WriteLine("Enter your choice");
+        int choice = int.Parse(Console.ReadLine());
+
+        Case caseObj = new Case();
+
+        if (choice == 1)
+        {
+            Console.WriteLine("Enter the case no");
+            caseObj.CaseNo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the case code");
+            caseObj.CaseCode = Console.ReadLine();
+
+            Console.WriteLine("Enter the case content");
+            caseObj.CaseContent = Console.ReadLine();
+
+            Console.WriteLine("Enter the date:");
+            caseObj.Date = DateTime.Parse(Console.ReadLine());
+
+            if (AddToList(caseObj))
+            {
+                Console.WriteLine("Added successfully");
+            } 
+        }else if (choice == 2)
+        {
+            Console.WriteLine("Enter the case no");
+            int caseNo = int.Parse(Console.ReadLine());
+
+            if (DeleteFromList(caseNo))
+            {
+                Console.WriteLine("Deleted successfully");
+            }
+            else
+            {
+                Console.WriteLine("Deleting failed");
+            } 
+        }
+    }
+
+    public static bool AddToList(Case caseObj)
+    {
+        CaseFile.Add(caseObj);
+        return true;
+        
+    }
+
+    public static bool DeleteFromList(int caseNo)
+    {
+        Case caseToRemove = CaseFile.Find(c => c.CaseNo == caseNo);
+
+        if(caseToRemove != null)
+        {
+            CaseFile.Remove(caseToRemove);
+            return true;
+        }
+        
+        return false;
+        
+    }
+}
