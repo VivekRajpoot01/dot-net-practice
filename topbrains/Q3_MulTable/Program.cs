044705507f7239daa78e0422769a4e754e8a9563
Vivek <vivekrajput2515@gmail.com>
2026-01-29 22:09:06 +0530

    add solution to Q3

---
+++ topbrains/Q3_MulTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    public int[] MultiplicationTable(int n, int upto)
+    {
+        int[] result = new int[upto];
+        for (int i = 1; i <= upto; i++)
+        {
+            result[i - 1] = n * i;
+        }
+        return result;
+    }
+
+    public static void Main()
+    {
+        // Read inputs
+        int n = int.Parse(Console.ReadLine());
+        int upto = int.Parse(Console.ReadLine());
+
+        // Create object to call non-static method
+        Program p = new Program();
+
+        int[] table = p.MultiplicationTable(n, upto);
+
+        // Print result
+        Console.WriteLine(string.Join(",", table));
+    }
+}
