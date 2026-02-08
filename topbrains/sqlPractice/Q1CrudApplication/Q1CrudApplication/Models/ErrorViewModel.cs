aac6700ea05aa96dc11132714781ad03c6659783
Vivek <vivekrajput2515@gmail.com>
2026-02-08 23:37:56 +0530

    add question1 for sql practice

---
+++ topbrains/sqlPractice/Q1CrudApplication/Q1CrudApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Q1CrudApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ topbrains/sqlPractice/Q1CrudApplication/Q1CrudApplication/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Q1CrudApplication.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public decimal? Cgpa { get; set; }
+}
