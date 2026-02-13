eb5523bb63840d9756a896741bf56ba95c5f7593
Vivek <vivekrajput2515@gmail.com>
2026-02-13 14:41:09 +0530

    add solution for question 1

---
+++ StringPrac/Q1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Dictionary<string,int> dict = new Dictionary<string, int>();
+        string inp = Console.ReadLine();
+
+        string[] words = inp.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+
+        foreach(string word in words)
+        {
+            char[] arr = word.ToCharArray();
+            string str = "";
+            foreach(char ch in arr)
+            {
+                
+                if (!char.IsPunctuation(ch))
+                {
+                    str += ch;
+                }
+            }
+            if (dict.ContainsKey(str))
+            {
+                dict[str] += 1;
+            }
+            else
+            {
+                dict[str] = 1;
+            }
+        }
+
+        foreach(var kv in dict)
+        {
+            Console.WriteLine($"{kv.Key}: {kv.Value}");
+        }
+
+    }
+}
