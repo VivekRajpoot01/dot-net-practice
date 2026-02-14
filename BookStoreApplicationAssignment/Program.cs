2c50d4960c2bda90ccc363aab79e861a44cece73
Vivek <vivekrajput2515@gmail.com>
2026-02-14 14:17:47 +0530

    add solution for the book  store application assignment

---
+++ BookStoreApplicationAssignment/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace BookStoreApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO:
+            // 1. Read initial input
+            // Format: BookID Title Price Stock
+
+            Book book = new Book();
+            string input = Console.ReadLine();
+            string[] arr = input.Split(' ');
+            book.Id = arr[0];
+            book.Title = arr[1];
+            book.Price = int.Parse(arr[2]);
+            book.Stock = int.Parse(arr[3]);
+
+            BookUtility utility = new BookUtility(book);
+
+            while (true)
+            {
+                // TODO:
+                // Display menu:
+                // 1 -> Display book details
+                // 2 -> Update book price
+                // 3 -> Update book stock
+                // 4 -> Exit
+
+                int choice = int.Parse(Console.ReadLine()); // TODO: Read user choice
+
+                switch (choice)
+                {
+                    case 1:
+                        utility.GetBookDetails();
+                        break;
+
+                    case 2:
+                        // TODO:
+                        // Read new price
+                        // Call UpdateBookPrice()
+                        int newPrice = int.Parse(Console.ReadLine());
+                        utility.UpdateBookPrice(newPrice);
+                        break;
+
+                    case 3:
+                        // TODO:
+                        // Read new stock
+                        // Call UpdateBookStock()
+                        int newStock = int.Parse(Console.ReadLine());
+                        utility.UpdateBookStock(newStock);
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Thank You");
+                        return;
+
+                    default:
+                        // TODO: Handle invalid choice
+                        break;
+                }
+            }
+        }
+    }
+}
