edc2c5803bd57f942fe18ed3647a466d9658fd3c
Vivek <vivekrajput2515@gmail.com>
2026-02-17 11:26:46 +0530

    add solution

---
+++ GenericCollections_Assessment/LibraryBookManagementSystem/Catalog.cs
@@ -0,0 +1,52 @@
+public class Catalog<T> where T : Book
+{
+    private List<T> _items = new List<T>();
+    private HashSet<string> _isbnSet = new HashSet<string>();
+    private SortedDictionary<string, List<T>> _genreIndex = new SortedDictionary<string, List<T>>();
+    
+    // Add item with genre indexing
+    public bool AddItem(T item)
+    {
+        // TODO: Check ISBN uniqueness, add to list and genre index
+        if (_isbnSet.Contains(item.ISBN))
+        {
+            return false;
+        }
+        _items.Add(item);
+        _isbnSet.Add(item.ISBN);
+
+        if (!_genreIndex.ContainsKey(item.Genre))
+        {
+            _genreIndex[item.Genre] = new List<T>();
+        }
+        _genreIndex[item.Genre].Add(item);
+        
+        return true;
+    }   
+    
+    // Get books by genre using indexer
+    public List<T> this[string genre]
+    {
+        get
+        {
+            // TODO: Return books by genre or empty list
+
+            if (_genreIndex.ContainsKey(genre))
+            {
+                return _genreIndex[genre];
+            }
+            return new List<T>();
+
+            //return _genreIndex.Where(kvp => kvp.Key == genre ).SelectMany(kvp => kvp.Value).ToList();
+
+        }
+    }
+    
+    // Find books using LINQ and lambda expressions
+    public IEnumerable<T> FindBooks(Func<T, bool> predicate)
+    {
+        // TODO: Use LINQ Where with predicate
+        return _items.Where(predicate);
+
+    }
+}
