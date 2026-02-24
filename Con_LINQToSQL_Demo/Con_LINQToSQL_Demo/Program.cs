2b79094896234a45d6f93cefd78b1d925eacc97a
Vivek <vivekrajput2515@gmail.com>
2026-02-24 22:47:34 +0530

    add

---
+++ Con_LINQToSQL_Demo/Con_LINQToSQL_Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Con_LINQToSQL_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product p1 = new Product()
+            {
+                ProdId = 201,
+                Name = "Tupperware Flask",
+                Category = "Container",
+                Price = 450,
+                Desc = "Virging Plastic for Water "
+
+            };
+            // Adding this object
+            LPUDbClassesDataContext.
+        }
+    }
+}
