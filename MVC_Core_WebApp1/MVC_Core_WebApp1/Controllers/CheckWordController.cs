f9476ad76d519847a79f04534c12c2877439f41f
Vivek <vivekrajput2515@gmail.com>
2026-02-25 22:10:25 +0530

    add check word controller to check a offensive word through action method

---
+++ MVC_Core_WebApp1/MVC_Core_WebApp1/Controllers/CheckWordController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Core_WebApp1.Controllers
+{
+    public class CheckWordController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        private static readonly HashSet<string> offensiveWords = new HashSet<string>()
+        {
+            "badmos",
+            "kutte",
+            "stupid",
+            "idiot",
+            "janwar",
+            "suar"
+        };
+
+        
+        public IActionResult WordChecker(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input))
+                return BadRequest("Input cannot be empty");
+
+            if (offensiveWords.Contains(input.ToLower()))
+                return Ok("Offensive word detected");
+
+            return Ok($"Clean word: {input}");
+        }
+    }
+}
